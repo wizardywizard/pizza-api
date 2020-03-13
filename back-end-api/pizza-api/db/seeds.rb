bcf556c642a4391a1a50e68c6a6e48e5db4708a6
wizardywizard <littledavid175@gmail.com>
2020-03-12 19:10:46 -0700

    created seed

--- back-end-api/pizza-api/db/seeds.rb
+++ back-end-api/pizza-api/db/seeds.rb
@@ -5,3 +5,114 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pizza.create([
+    {
+        name: "the big cheese",
+        sause: "cheese sauce",
+        cheese: "extra cheese"
+    },
+    {
+        name: "the reg",
+        sause: "pizza sauce",
+        cheese: "mozeriala cheese"
+    }
+])
+
+Topping.create([
+    {
+        name: "Ham",
+        meat: true,
+        vegetable: false
+    },
+    {
+        name: "Beef",
+        meat: true,
+        vegetable: false  
+    },
+    {
+        name: "Salami",
+        meat: true,
+        vegetable: false 
+    },
+    {
+        name: "Pepperoni",
+        meat: true,
+        vegetable: false
+    },
+    {
+        name: "Italian Sausage",
+        meat: true,
+        vegetable: false
+    },
+    {
+        name: "Premium Chicken",
+        meat: true,
+        vegetable: false
+    },
+    {
+        name: "Bacon",
+        meat: true,
+        vegetable: false 
+    },
+    {
+        name: "Philly Steak",
+        meat: true,
+        vegetable: false
+    },
+    {
+        name: "Garlic",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Jalapeno",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Onions",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Banana Peppers",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Diced Tomatos",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Black Olives",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Mushrooms",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Pineapple",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Green Peppers",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Spinach",
+        meat: false,
+        vegetable: true
+    },
+    {
+        name: "Red Peppers",
+        meat: false,
+        vegetable: true
+    }
+])
