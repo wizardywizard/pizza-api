8f9d06623f8310fae8a2798607ef9e444cf6d27d
wizardywizard <littledavid175@gmail.com>
2020-03-16 17:04:03 -0700

    worked on client side

--- front-end-client/src/index.js
+++ front-end-client/src/index.js
@@ -1,5 +1,8 @@
-class Pizza{
-    constructor(attributes){
-        
+class PizzaAPI{
+    static getPizza(){
+        return fetch(`${PizzaAPI.base_url}/pizzas`).then(res => res.json())
     }
+
+
 }
+PizzaAPI.base_url = "http://localhost:3000"
