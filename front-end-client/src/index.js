83948f34ba0539142fe6501d2edbc9f59b5d2fd6
wizardywizard <littledavid175@gmail.com>
2020-03-18 18:44:35 -0700

    complete pizzaApi

--- front-end-client/src/index.js
+++ front-end-client/src/index.js
@@ -7,9 +7,65 @@ class PizzaAPI{
         return fetch(`${PizzaAPI.base_url}/pizzas/${pizzaId}`)
         .then(res => res.json())
         .then(json => {
-            debugger
+            const {
+                data: {
+                    id,
+                    attributes: {
+                        name,
+                        sauce,
+                        cheese,
+                    }
+                },
+                included
+            } = json
+            return {
+                id,
+                name,
+                sauce,
+                cheese,
+                toppings: included.map(({id, attributes: {name, meat, pizza_id}}) =>{
+                    return {
+                        id,
+                        name,
+                        meat,
+                        pizza_id
+                    }
+                })
+            }
         })
     }
 
+    static createPizza(pizzaAttributes) {
+        return fetch(`${PizzaAPI.base_url}/pizzas`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(pizzaAttributes)
+        })
+        .then(res => res.json())
+    }
 }
 PizzaAPI.base_url = "http://localhost:3000"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
