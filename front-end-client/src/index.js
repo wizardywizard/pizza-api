2a715da5772f32bc7abd186ca32ec50ac3b7dc77
wizardywizard <littledavid175@gmail.com>
2020-03-20 19:19:55 -0700

    added event listners to project

--- front-end-client/src/index.js
+++ front-end-client/src/index.js
@@ -208,15 +208,45 @@ class PizzaShowPage {
     ${this.renderTrackList()}
     `
     }
+}
 
 // Event Listeners
 
+document.addEventListener('DOMContentLoaded', () => {
+    let root = document.getElementById('root')
+    root.innerHTML = loadingGif()
+    Pizza.getAll().then(pizzas => {
+        root.innerHTML = new PizzaPage(pizzas.render())
+    })
+    document.addEventListener('click', (e) => {
+        if(e.target.matches(".pizzaShow")) {
+            let pizza = Pizza.findById(e.target.dataset.pizzaid)
+            pizza.getPizzaDetails().then(pizza => {
+                root.innerHTML = new PizzaShowPage(pizza).render()
+            })
+        }
+        if(e.target.matches('.pizzasIndex')) {
+            root.innerHTML = new PizzaPage(pizza.all).render()
+        }
+    })
+    document.addEventListener('submit', (e) => {
+        e.preventDefault()
+        if(e.target.matches('.addPizza')) {
+          let formData = {}
+          e.target.querySelectorAll('input[type="text"]').forEach(input => formData[input.id] = input.value)
+          Pizza.create(formData)
+            .then(pizza => {
+              document.querySelector('#pizzas').insertAdjacentHTML('beforeend', pizza.renderCard())
+            })
+        }
+    })
+})
 
-}
-
-
-
-
+const loadingGif = () => {
+    let loading = document.createElement('img')
+    loading.src = 'https://i.giphy.com/media/y1ZBcOGOOtlpC/giphy.webp'
+    return loading.outerHTML
+  }
 
 
 
