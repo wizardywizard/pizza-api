d2bb1d2ff483a4d7643a5b536e26e5508a1f5c72
wizardywizard <littledavid175@gmail.com>
2020-03-25 18:02:57 -0700

    added sauce and cheese to pizza show page

--- front-end-client/src/index.js
+++ front-end-client/src/index.js
@@ -209,6 +209,8 @@ class PizzaShowPage {
         return `    
     <div class="ph2 ph0-ns pb3 link db">
       <h3 class="f5 f4-ns mb0 black-90">${this.pizza.name}</h3>
+      <h3 class="f5 f4-ns mb0 black-90">${this.pizza.sauce}</h3>
+      <h3 class="f5 f4-ns mb0 black-90">${this.pizza.cheese}</h3>
     </div>
     ${this.renderToppingPizza()}
     `
